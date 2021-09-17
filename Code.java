15bf7bdd87accedafcd9e4172412c3cfc5d5cc78
Ali <60489392+aaman6@users.noreply.github.com>
2021-09-17 12:48:59 -0500

    Create Code.java

---
+++ Code.java
@@ -0,0 +1,10 @@
+Public class Code
+{
+public static void main (String args [] )
+  {    
+
+      System.out.println("Hello World");
+      System.out.println("Code");
+      System.out.println("Testing");
+  }
+}
