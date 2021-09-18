c10fba65b5155f0aaa45b00f289fd08026236f13
Ali <60489392+aaman6@users.noreply.github.com>
2021-09-17 23:48:40 -0500

    Update Code.java

--- Code.java
+++ Code.java
@@ -2,7 +2,9 @@ Public class Code
 {
 public static void main (String args [] )
   {    
-
+      for (int i=0; i<10; i++)
+      System.out.println(i);
+  
       System.out.println("Hello World");
       System.out.println("Code");
       System.out.println("Testing");
